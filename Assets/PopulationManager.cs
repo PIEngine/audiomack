2a2c827b77c3ad2759cce7728c3b836c179a9e52
PIEngine <113545534+PIEngine@users.noreply.github.com>
2021-09-15 22:56:29 +0000

    small style fixes

--- Assets/PopulationManager.cs
+++ Assets/PopulationManager.cs
@@ -11,4 +11,4 @@ public class PopulationManager : MonoBehaviour {
 
 	void Start(){
 		for(int i =0; i< populationSize; i++){
-			Vector3 pos = new Vector3(Random.Rang
+			Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0); // Returns Min(i
