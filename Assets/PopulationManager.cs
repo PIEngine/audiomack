1e0b3a4f94ff532138307c63ac2582c9b65c8b1b
PIEngine <113545534+PIEngine@users.noreply.github.com>
2021-09-18 10:53:46 +0000

    5.4.3

--- Assets/PopulationManager.cs
+++ Assets/PopulationManager.cs
@@ -11,4 +11,5 @@ public class PopulationManager : MonoBehaviour {
 
 	void Start(){
 		for(int i =0; i< populationSize; i++){
-			Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0); // Returns Min(i
+			Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0); // Returns Min(inclusive) and Max(exclusive)
+			GameObject go = Instant
