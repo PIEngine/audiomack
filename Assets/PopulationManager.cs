1f8875746a72061cbebb2f4a5067a273012b9599
PIEngine <113545534+PIEngine@users.noreply.github.com>
2021-09-18 12:43:06 +0000

    Update install_docker.sh

--- Assets/PopulationManager.cs
+++ Assets/PopulationManager.cs
@@ -12,4 +12,4 @@ public class PopulationManager : MonoBehaviour {
 	void Start(){
 		for(int i =0; i< populationSize; i++){
 			Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0); // Returns Min(inclusive) and Max(exclusive)
-			GameObject go = Instant
+			GameObject go = Instantiate(personPrefab, pos, Quaternion.id
